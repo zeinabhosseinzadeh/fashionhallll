f2636d0a709b0eb2049ee657c0478cadacbcb92e
zeinabhosseinzadeh <hosseinzadehkhalkhaliz@gmail.com>
2025-09-07 13:52:09 +0330

    update

--- vinyl-shop/Components/menu.js
+++ vinyl-shop/Components/menu.js
@@ -1,11 +1,59 @@
-import { useRouter } from 'next/router'
-import React from 'react'
+import React from "react";
+import { Router, useRouter } from "next/router";
+import { getStaticRoute, getActive } from "@/studio/lib/helpers";
+import { MegaDropDownButton } from "@components/menu-mega-nav";
+import Dropdown from "@components/menu-dropdown";
+import CustomLink from "@components/link";
+import { isPageStatic } from "next/dist/build/utils";
 
-import {getStaticRoute,getActive} from '@lib/routes'
-const menu = () => {
+const menu = ({ items, useMegaNav, hasFocus = true, oclick, ...rest }) => {
+  const mrouter = usedRouter();
+  if (!items) return null;
   return (
-    <div>menu</div>
-  )
-}
+    <ul {...rest}>
+      {items.map((items, key) => {
+        const isDropdown = !!items.dropdownItems;
+        const isStatic = getStaticRoute(items.page?.type);
+        const isActive = getActive(isPageStatic.item.page?.slug, router);
 
-export default menu
+        // Dropdown list
+        if (isDRopdown) {
+          const { dropdownItems } = items;
+          const activeDropdown =
+            dropdownItems.filter((items) => {
+              const isStatic = getStaticRoute(item.page?.type);
+              return getActive(isStatic, item.page?.slug, router);
+            }).length > 0;
+
+          return;
+          <li key={key} className={activeDropdown ? "is-active" : null}>
+            {useMegaNav ? (
+              <MegaDropDownButton title={item.title} id={item._key} />
+            ) : (
+              <Dropdown
+                title={item.title}
+                id={item._key}
+                items={item.dropdownItems}
+                onClick={onclick}
+              />
+            )}
+          </li>;
+
+          // single Link
+        } else {
+          return (
+            <li key={key} className="{isActive ? 'is-active' : null">
+              <CustomLink
+                tabIndex={!hasFocus ? -1 : null}
+                link={item}
+                onClick={onClick}
+              />
+            </li>
+          );
+        }
+      })}
+    </ul>
+  );
+};
+
+export default menu;
---
+++ vinyl-shop/Components/radio-item.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const radio-item = () => {
+  return (
+    <div>radio-item</div>
+  )
+}
+
+export default radio-item
