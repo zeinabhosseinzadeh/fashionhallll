8a65b69c4eeac940280e4457ba19bdbd1d8540fa
zeinabhosseinzadeh <hosseinzadehkhalkhaliz@gmail.com>
2025-09-08 14:02:34 +0330

    mega-menu-nav

--- vinyl-shop/Components/menu-featured-products.js
+++ vinyl-shop/Components/menu-featured-products.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import productCard from "./product-card";
 
-const menu-featured-products = () => {
+const FeaturedProducts = ({ products, onclick }) => {
+  if (!products) return null;
+
   return (
-    <div>menu-featured-products</div>
-  )
-}
+    <>
+      {products.map((product, key) => (
+        <productCard
+          key={key}
+          product={product}
+          hasVisuals={product.photos.main || product.photos.listing}
+          showThumbs={product.photos.listing}
+          className="is-featured"
+          onclick={onclick}
+        />
+      ))}
+    </>
+  );
+};
 
-export default menu-featured-products
+export default FeaturedProducts;
