ffe1dd93f7426df9e2d4231042eccfd2df67cd72
zeinabhosseinzadeh <hosseinzadehkhalkhaliz@gmail.com>
2025-09-13 21:27:57 +0330

    update accordion/vimeo/promo/accordionlist

--- vinyl-shop/Components/accordion-list.js
+++ vinyl-shop/Components/accordion-list.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const accordion-list = () => {
+import Accordion from '@components/accordion'
+import BlockContent from '@components/block-content'
+
+const AccordionList = ({ data }) => {
+  const { items } = data
+
+  const [activeAccordion, setActiveAccordion] = useState(null)
+
+  const onToggle = (id, status) => {
+    setActiveAccordion(status ? id : null)
+  }
+
   return (
-    <div>accordion-list</div>
+    <div className="accordion-group">
+      {items.map((accordion, key) => {
+        return (
+          <Accordion
+            key={key}
+            id={accordion.id}
+            isOpen={accordion.id === activeAccordion}
+            onToggle={onToggle}
+            title={accordion.title}
+          >
+            <BlockContent blocks={accordion.content} />
+          </Accordion>
+        )
+      })}
+    </div>
   )
 }
 
-export default accordion-list
+export default AccordionList
--- vinyl-shop/Components/accordion.js
+++ vinyl-shop/Components/accordion.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { Children, useState } from 'react'
+import { m } from 'framer-motion'
+import cx from 'classnames'
+import Icon from '@components/icon'
+import classNames from 'classnames'
+import accordion from '@/studio/schemas/objects/accordion'
 
-const accordion = () => {
-  return (
-    <div>accordion</div>
-  )
+const accordionAnim ={
+  open:{
+    opacity:1,
+    height:'auto'
+  },
+  closed:{
+    opacity:0,
+    height:0,
+  },
 }
 
-export default accordion
+const Accordion = ({
+  id,
+  title,
+  isOpen= false,
+  isControlled=false,
+  ontoggle = () =>{},
+  className,
+  Children,
+}) => {
+  const [hasFocus, setHasFocus] = useState(isOpen)
+
+  return(
+<div key={id} className={cx('accordion', className)}>
+  {!isControlled && (
+    <button 
+    onClick={()=> ontoggle(id,!isOpen)}
+    aria-expanded={isOpen}
+    aria-controls={`accordion-${id}`}
+    className={cx('accordion--toggle', {'is-open':isOpen})}
+    >
+      {title}
+      <div className="accordion--icon">
+        <Icon name= "Chevron Down" />
+      </div>
+    </button>
+  )}
+  <m.div 
+  id={`accordion-${id}`}
+  className='accordion--content'
+  initial={isOpen? 'open' :'closed'}
+  animate={isOpen? 'open' :'closed'}
+  variants={accordionAnim}
+  transition={{duration:0.5, ease: [0.19, 1.0,0.22,1.0] }}
+  onAnimationComplete={(v) => setHasFocus(v === 'open')}
+  >
+      <div className="accordion--inner" hidden={!isOpen && !hasFocus}>
+          {children}
+        </div>
+  </m.div>
+</div>
+  )
+}
+export default Accordion  
