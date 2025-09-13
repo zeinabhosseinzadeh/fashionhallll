5ae4f89dc00eaa1ec3d4662dd6546035d5330cf3
zeinabhosseinzadeh <hosseinzadehkhalkhaliz@gmail.com>
2025-09-13 21:36:56 +0330

    update block-content

--- vinyl-shop/Components/block-content.js
+++ vinyl-shop/Components/block-content.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import cx from 'classnames'
+import BlockContent from '@sanity/block-content-to-react'
 
-const block-content = () => {
+import { blockSerializers } from '@components/block-serializers'
+
+const Content = ({ blocks, className }) => {
+  if (!blocks) return null
+
   return (
-    <div>block-content</div>
+    <BlockContent
+      renderContainerOnSingleChild
+      className={cx('rc', className)}
+      blocks={blocks}
+      serializers={blockSerializers}
+    />
   )
 }
-
-export default block-content
+export default Content
