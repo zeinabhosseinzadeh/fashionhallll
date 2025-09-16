22860c9531b6dc52dd0ebfa0959cccd6b2309ba3
zeinabhosseinzadeh <hosseinzadehkhalkhaliz@gmail.com>
2025-09-16 10:48:37 +0330

    upload chip.js

--- vinyl-shop/Components/chip.js
+++ vinyl-shop/Components/chip.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import cx from 'classnames'
 
-const chip = () => {
+const Chip = ({
+  avatar,
+  label,
+  icon,
+  title,
+  onClick = () => {},
+  className,
+  children,
+}) => {
+  if (!children) return null
+
+  const handleClick = () => {
+    onClick()
+  }
+
   return (
-    <div>chip</div>
+    <button
+      onClick={handleClick}
+      aria-label={label}
+      title={title}
+      className={cx('chip', className)}
+    >
+      {avatar && <span className="chip--avatar">{icon}</span>}
+      <span className="chip--label">{children}</span>
+      {icon && <span className="chip--icon">{icon}</span>}
+    </button>
   )
 }
 
-export default chip
+export default Chip
