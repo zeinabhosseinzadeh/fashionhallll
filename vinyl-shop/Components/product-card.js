b9699e75c36e3976ebe5d5c2da2d47e5af7ba1b3
zeinabhosseinzadeh <hosseinzadehkhalkhaliz@gmail.com>
2025-09-16 10:52:45 +0330

    upload product-card

--- vinyl-shop/Components/product-card.js
+++ vinyl-shop/Components/product-card.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { m } from 'framer-motion'
+import Link from 'next/link'
+import cx from 'classnames'
 
-const product-card = () => {
-  return (
-    <div>product-card</div>
-  )
+import { hasObject } from '@lib/helpers'
+
+import {
+  ProductGallery,
+  ProductThumbnail,
+  ProductPrice,
+  ProductOption,
+  ProductAdd,
+} from '@components/product'
+
+const itemAnim = {
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      ease: 'linear',
+    },
+  },
+  hide: {
+    opacity: 0,
+    transition: {
+      duration: 0.3,
+      ease: 'linear',
+    },
+  },
 }
 
-export default product-card
+export default ProductCard
