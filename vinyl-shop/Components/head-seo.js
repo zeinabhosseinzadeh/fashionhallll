5018e0884a18a7addc02d84958cf593fa4bad0ca
zeinabhosseinzadeh <hosseinzadehkhalkhaliz@gmail.com>
2025-09-18 13:36:12 +0330

    update head-seo.js

--- vinyl-shop/Components/head-seo.js
+++ vinyl-shop/Components/head-seo.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import Head from 'next/head'
+import { imageBuilder } from '@lib/sanity'
+import schema from '@/studio/schemas/schema'
 
-const head-seo = () => {
-  return (
-    <div>head-seo</div>
-  )
+
+const HeadSEO = ({site ={},page ={} schema }) =>{
+    // set <head> variables
+    const siteTitle = site.siteTitle
+
+  const siteFavicon = site.seo?.favicon || '/favicon.svg'
+  const siteFaviconLegacy = site.seo?.faviconLegacy || '/favicon.ico'
+  const siteTouchIcon = site.seo?.touchIcon
+
+  const templateTags = [
+    {
+      tag: '{{page_title}}',
+      value: page.title,
+    },
+    {
+      tag: '{{site_title}}',
+      value: siteTitle,
+    },
+  ]
+
+
+
 }
 
-export default head-seo
+export default HeadSEO
