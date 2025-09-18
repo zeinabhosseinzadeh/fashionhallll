35b2740962174e0cb59bbf6ae7672f4ed7dd6646
zeinabhosseinzadeh <hosseinzadehkhalkhaliz@gmail.com>
2025-09-18 13:59:02 +0330

    update layout

--- vinyl-shop/Components/layout.js
+++ vinyl-shop/Components/layout.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState, memo } from "react";
+import Script from "next/script";
+import { m } from "framer-motion";
+import { isBrowser, isMobileSafari, useWindowSize } from "@lib/helpers";
+import { pageTransitionSpeed } from "@lib/animate";
+import HeadSEO from "@components/head-seo";
+import CookieBar from "@components/cookie-bar";
+import Header from "@components/header";
+import Footer from "@components/footer";
 
-const layout = () => {
-  return (
-    <div>layout</div>
-  )
-}
-
-export default layout
+export default layout;
